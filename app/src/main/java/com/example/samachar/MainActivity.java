b897a87f6d61654421f464f1bcd4f49002445611
Basavraj <basavrajbirajdar2001@gmail.com>
2022-09-22 00:48:00 +0530

    Pre-Final

--- app/src/main/java/com/example/samachar/MainActivity.java
+++ app/src/main/java/com/example/samachar/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.samachar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView img;
+    private Animation topanimation;
+    private static int SPLASH_SCREEN=4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        img=findViewById(R.id.Appname);
+        topanimation= AnimationUtils.loadAnimation(this,R.anim.topanim);
+        img.setAnimation(topanimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, HomeScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
     }
 }
